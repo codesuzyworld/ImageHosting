cd4e8f829c7fc46d29f8229e1ee04354df7982a3
codesuzyworld <codessuzy@gmail.com>
2024-09-25 15:10:54 -0400

    Add project files.

---
+++ ImageHosting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageHosting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
